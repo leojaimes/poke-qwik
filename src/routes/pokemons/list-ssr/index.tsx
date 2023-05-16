1d909c95de6ffc330d9555630119ed98f2baa832
leo.jaimes <leo777jaimes@gmail.com>
2023-05-16 11:24:28 -0600

    upload

--- src/routes/pokemons/list-ssr/index.tsx
+++ src/routes/pokemons/list-ssr/index.tsx
@@ -1,34 +1,45 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useComputed$, useSignal } from '@builder.io/qwik';
 
-import { type DocumentHead, Link, routeLoader$ } from '@builder.io/qwik-city';
+import { type DocumentHead, Link, routeLoader$, useLocation } from '@builder.io/qwik-city';
 import { getPokemoms } from '~/services/pokemonApi';
 
 const usePokemonList = routeLoader$(async ({ params, query, redirect }) => {
+    const offset = query.get('offset')
+
     const res = await getPokemoms({
         limit: 10,
-        offset: 0
+        offset: Number(offset)
     })
-    return res
+    return {
+        res,
+        offset: Number(offset),
+    }
 })
 export default component$(() => {
-    const { value: { results } } = usePokemonList()
+    const { value: { offset, res } } = usePokemonList()
+    const location = useLocation()
+    const currentoffset = useComputed$(() => {
+        const offsetString = new URLSearchParams(location.url.search)
+        return Number(offsetString.get('offset') || 0)
+    })
     return (
         <>
             <div class="flex flex-col ">
                 <span class="my-5 text-5xl">Status</span>
-                <span class="my-5 text-5xl">Current offset: XXXX</span>
+                <span class="my-5 text-5xl">Current offset: {currentoffset}</span>
                 <span class="my-5 text-5xl">Loading page</span>
             </div>
             <div class="mt-10">
-                <Link class="btn btn-primary mr-2"> Previous </Link>
-                <Link class="btn btn-primary mr-2"> Next </Link>
+                <Link class="btn btn-primary mr-2 cursor-pointer" href={`/pokemons/list-ssr/?offset=${offset - 1 >= 0 ? offset - 1 : 0}`}> Previous </Link>
+                <Link class="btn btn-primary mr-2 cursor-pointer" href={`/pokemons/list-ssr/?offset=${offset + 1}`} > Next </Link>
             </div>
             <div class="grid grid-cols-6 mt-5">
                 {
-                    results.map(({ name, url }, index) => (
+                    res.results.map(({ name }, index) => (
                         <>
-                            <div class="m-5 flex flex-col justify-center items-center">{name}</div>
-                            <img src={url} alt={name} />
+                            <div key={`${name} - ${index}`} class="m-5 flex flex-col justify-center items-center">
+                                <span class="capitalize">{name}</span>
+                            </div>
                         </>
                     ))
                 }
